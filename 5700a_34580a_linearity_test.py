bd9a6ceeb503b2536c018e78592ececf0b0bc163
reps <marco@reps.cc>
2025-10-29 19:13:48 +0100

    Update 5700a_34580a_linearity_test.py

--- 5700a_34580a_linearity_test.py
+++ 5700a_34580a_linearity_test.py
@@ -86,14 +86,16 @@ def one_sweep(instruments, source, filename):
         
 
 
-instruments["3458B"]["inst"]=setup_3458a('TCPIP::192.168.0.5::gpib0,23')
-instruments["3458P"]["inst"]=setup_3458a('TCPIP::192.168.0.5::gpib0,22')
-instruments["3458H"]["inst"]=setup_3458a('gpib0::21::INSTR')
-instruments['3458A_MY45054264']["inst"]=setup_3458a('gpib0::2::INSTR')
-instruments['3458A_US28028957']["inst"]=setup_3458a('gpib0::24::INSTR')
-instruments['3458A_MY59352556']["inst"]=setup_3458a('gpib0::22::INSTR')
-instruments['3458A_2823A25425']["inst"]=setup_3458a('gpib0::5::INSTR')
-instruments['34470A']["inst"]=setup_34470a('TCPIP::192.168.0.103::inst0::INSTR')
-source=setup_5700a('GPIB0::1::INSTR')
+instruments["3458B"] = {'inst': setup_3458a('TCPIP::192.168.0.5::gpib0,23'), "results": [0] * n_test_points}
+instruments["3458P"] = {'inst': setup_3458a('TCPIP::192.168.0.5::gpib0,22'), "results": [0] * n_test_points}
+instruments["3458H"] = {'inst': setup_3458a('gpib0::21::INSTR'), "results": [0] * n_test_points}
+instruments['3458A_MY45054264'] = {'inst': setup_3458a('gpib0::2::INSTR'), "results": [0] * n_test_points}
+instruments['3458A_US28028957'] = {'inst': setup_3458a('gpib0::24::INSTR'), "results": [0] * n_test_points}
+instruments['3458A_MY59352556'] = {'inst': setup_3458a('gpib0::22::INSTR'), "results": [0] * n_test_points}
+instruments['3458A_2823A25425'] = {'inst': setup_3458a('gpib0::5::INSTR'), "results": [0] * n_test_points}
+instruments['34470A'] = {'inst': setup_34470a('TCPIP::192.168.0.103::inst0::INSTR'), "results": [0] * n_test_points}
+
+source = setup_5700a('GPIB0::1::INSTR')
+
 
 one_sweep(instruments, source)
